14f5d9fb371bbfc9c445fe837643652a28078cae
Miguel-player-09 <121067934+Miguel-player-09@users.noreply.github.com>
2023-05-11 22:33:41 -0300

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,12 +1,11 @@
-//adicione o código do seu banco de dados
 const firebaseConfig = {
-  apiKey: "AIzaSyALAnJV1JezO-Bi1RiZAmuLcfCqtgwwppc",
-  authDomain: "kwitterbd-c9b27.firebaseapp.com",
-  databaseURL: "https://kwitterbd-c9b27-default-rtdb.firebaseio.com",
-  projectId: "kwitterbd-c9b27",
-  storageBucket: "kwitterbd-c9b27.appspot.com",
-  messagingSenderId: "765789229419",
-  appId: "1:765789229419:web:1f8efde36d26b887c4bc2e"
+  apiKey: "AIzaSyC_IU4AvZd2sNsnmgaV1ujOcIZVuWZC6dM",
+  authDomain: "kwitter-db-add40.firebaseapp.com",
+  databaseURL: "https://kwitter-db-add40-default-rtdb.firebaseio.com",
+  projectId: "kwitter-db-add40",
+  storageBucket: "kwitter-db-add40.appspot.com",
+  messagingSenderId: "589339952433",
+  appId: "1:589339952433:web:66141cf5466b923e64dc10"
 };
 
 //// Inicializar Firebase
